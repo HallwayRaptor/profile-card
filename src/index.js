1f1aaa56016ab9abd76ae5af675cb61250edf14d
Rob Whelan <hallwayraptor@gmail.com>
2023-06-15 14:53:09 -0300

    convert skills to object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const skills = [
+	{
+		skill: "HTML + CSS",
+		level: "advanced",
+		color: "#E67E80",
+	},
+	{
+		skill: "Javascript",
+		level: "intermediate",
+		color: "#E69875",
+	},
+	{
+		skill: "Node",
+		level: "intermediate",
+		color: "#DBBC7F",
+	},
+	{
+		skill: "React",
+		level: "beginner",
+		color: "#D699B6",
+	},
+	{
+		skill: "Git + Github",
+		level: "intermediate",
+		color: "#83C092",
+	},
+];
 
 const App = () => (
 	<div className="card">
@@ -9,32 +37,45 @@ const App = () => (
 			<Description />
 		</div>
 		<div className="skill-list">
-			<Skill name="HTML + CSS" color="lightblue" />
-			<Skill name="Javascript" color="lightcoral" />
-			<Skill name="Node" color="lightgreen" />
-			<Skill name="Git + Github" color="pink" />
+			{skills.map((skill) => (
+				<Skill skillObj={skill} key={skill.name} />
+			))}
 		</div>
 	</div>
 );
 
-const Avatar = () =>
-  <img className='avatar' src="avatar.png" alt="Raptor" srcset="" />
+const Skill = ({ skillObj }) => {
+	const level = {
+		advanced: "🌳",
+		intermediate: "🌿",
+		beginner: "🌱",
+	};
 
-const Description = () => 
-  <div>
-    <h1>hallwayRaptor</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, fugiat! Consequuntur velit molestias corrupti eligendi dolore repellendus, repellat deleniti totam.</p>
-  </div>
+	return (
+		<div className="skill" style={{ backgroundColor: skillObj.color }}>
+			<span>{skillObj.skill}</span>
+			<span>{`${level[skillObj.level]}`}</span>
+		</div>
+	);
+};
 
-const Skill = (props) => (
-  <div className="skill" style={{backgroundColor: props.color}}><span>{props.name}</span></div>
+const Avatar = () => (
+	<img className="avatar" src="avatar.png" alt="Raptor" srcset="" />
 );
 
+const Description = () => (
+	<div>
+		<h1>hallwayRaptor</h1>
+		<p>
+			Full-stack web developer. When not coding I like to produce synthwave
+			music, play fighting games, and exercise.
+		</p>
+	</div>
+);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
